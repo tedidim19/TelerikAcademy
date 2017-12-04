ea526d35f25e257945b08750b50310a0830cc9c0
Teodora Dimitrova <tedidim17@gmail.com>
2017-12-04 14:02:28 +0200

    Create DeclareVariables.java

---
+++ DeclareVariables.java
@@ -0,0 +1,21 @@
+package Main;
+
+public class First {
+
+	public static void main(String[] args)
+	{
+		byte byteNumNeg = -115;
+		byte byteNumPos =97;
+		short shortNum = -10000;
+		int intNum = 52130;
+		long longNum = 4825932;
+		
+		System.out.println(byteNumNeg);
+		System.out.println(byteNumPos);
+		System.out.println(shortNum);
+		System.out.println(intNum);
+		System.out.println(longNum);
+	}
+}
+	 
+
