041fbf80bfa29aad5ecf914b8783609909150fe1
Teodora Dimitrova <tedidim17@gmail.com>
2017-12-04 15:10:00 +0200

    Create IsFemale.java

---
+++ IsFemale.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class First {
+	
+	public static void main(String[] args)
+	{
+	
+		boolean isFemale=true;
+		
+		System.out.println("I am a female: "+ isFemale);
+	}
+}
+	 
+
