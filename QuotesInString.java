f36aab286614bb8d5ab85455702076fb33c0a5ab
Teodora Dimitrova <tedidim17@gmail.com>
2017-12-04 15:15:36 +0200

    Create QuotesInString.java

---
+++ QuotesInString.java
@@ -0,0 +1,14 @@
+package Main;
+
+public class First {
+	
+	public static void main(String[] args)
+	{
+	
+		String sent = "The \"use\" of quotations causes difficulties.";
+	
+		System.out.println(sent);
+	}
+}
+	 
+
